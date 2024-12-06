5d6518727aa5d7e42289f21fc6dd6c173fa47444
Josiah <josiahpryoredu@gmail.com>
2024-12-06 18:43:23 -0500

    finished web gathering

--- webLogin&Scrape.py
+++ webLogin&Scrape.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 import time
 
@@ -24,31 +25,50 @@ try:
     # Wait for JavaScript to load (use WebDriverWait for more robust handling)
     time.sleep(3)  # You may need to adjust this depending on the page's load time
 
-
     # This element is inside 6 nested shadow DOMs.
-    css_selector_for_host1 = "#d2l_1_15_896"
+    #css_selector_for_host1 = "d2l_1_15_896" # DWH - This number changes every time the page loads.  So when the addon shows it as _896, the next load it might be _360
     css_selector_for_host2 = "d2l-my-courses-container"
-    css_selector_for_host3 = "d2l-tabs:nth-child(7) > d2l-tab-panel:nth-child(3) > d2l-my-courses-content:nth-child(1)"
+    css_selector_for_host3 = "d2l-tabs > d2l-tab-panel:nth-child(3) > d2l-my-courses-content" #only works with current semester
+    
+     # This, too, is going to be specific to how many classes a person has.  Might need to find a more generic tag to search on, instead of the CSS "d2l-tabs:nth-child(3) > d2l-tab-panel:nth-child(1) > d2l-my-courses-content:nth-child(1)"
     css_selector_for_host4 = "d2l-my-courses-card-grid"
-    css_selector_for_host5 = "d2l-enrollment-card[href='https://b342f0f7-3270-489f-989b-9d9a2993382c.enrollments.api.brightspace.com/enrolled-user/uYvgzO3OgKP7aqqAgNARJiGl_0nNmt68TX2I6yMJ1h0/enrollment?localeId=1']"
-    css_selector_for_host6 = "d2l-card[text='202430 - CIS 332 01 - Web Programming I, off.202430.30093, 202430 - Fall 2024-2025, Ends December 21, 2024 at 12:00 AM']"
+    # "d2l-my-courses-card-grid"
+    css_selector_for_host5 = ".course-card-grid.columns-2 > d2l-enrollment-card:nth-child(2)" # different enrollment cards for diff. classes
+    css_selector_for_host6 = "d2l-card" # Same as before - this is a very specific tag to a very specific class.
+
+
 
 
 
     time.sleep(1)
-    shadow0 = driver.find_element(By.CSS_SELECTOR, css_selector_for_host1).shadow_root
-    # time.sleep(1)
-    # shadow1 = shadow0.find_element(By.CSS_SELECTOR, css_selector_for_host2).shadow_root
-    # time.sleep(1)
-    # shadow2 = shadow1.find_element(By.CSS_SELECTOR, css_selector_for_host3).shadow_root
-    # time.sleep(1)
-    # shadow3 = shadow2.find_element(By.CSS_SELECTOR, css_selector_for_host4).shadow_root
-    # time.sleep(1)
-    # shadow4 = shadow3.find_element(By.CSS_SELECTOR, css_selector_for_host5).shadow_root
-    # time.sleep(1)
-    # shadow5 = shadow4.find_element(By.CSS_SELECTOR, css_selector_for_host6).shadow_root
-    # time.sleep(1)
-    # shadow5.find_element(By.CSS_SELECTOR, "a[href='/d2l/home/34862']")
+    elements1 = driver.find_elements(By.TAG_NAME, "d2l-my-courses")
+    for e in elements1:
+        shadow0 = e.shadow_root
+        time.sleep(1)
+        shadow1 = shadow0.find_element(By.CSS_SELECTOR, css_selector_for_host2).shadow_root
+        time.sleep(1)
+        shadow2 = shadow1.find_element(By.CSS_SELECTOR, css_selector_for_host3).shadow_root
+        time.sleep(1)
+        shadow3 = shadow2.find_element(By.CSS_SELECTOR, css_selector_for_host4).shadow_root
+        time.sleep(1)
+
+        classes = shadow3.find_elements(By.CSS_SELECTOR, "d2l-enrollment-card")
+        num_classes = len(classes)
+        print(num_classes)
+        class_num = 1
+        all_class_text = ""
+        for Class in classes:
+            shadow4 = shadow3.find_element(By.CSS_SELECTOR, f".course-card-grid.columns-2 > d2l-enrollment-card:nth-child({class_num})").shadow_root
+            time.sleep(1)
+            class_num += 1
+            
+            class_text = shadow4.find_element(By.CSS_SELECTOR, "d2l-card").text
+            all_class_text += class_text
+            
+        
+        # shadow5 = shadow4.find_element(By.CSS_SELECTOR, css_selector_for_host6).shadow_root
+        # time.sleep(1)
+        # shadow5.find_element(By.CSS_SELECTOR, "a[href='/d2l/home/35189']")
 
 
     
