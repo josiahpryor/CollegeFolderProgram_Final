6356f422d811f793189c880b6048263320b8908d
Josiah <josiahpryoredu@gmail.com>
2024-12-06 21:43:37 -0500

    created main

---
+++ main.py
@@ -0,0 +1,10 @@
+from webLoginAndScrape import *
+
+def main():
+    class_list = collectClasses()
+    print(class_list)
+
+    sample = "{'COMM 202 06 - Fund of Speech', 'CIS 321 01 - Data & File Structures', 'CIS 302 01 - Windows Programming', 'CIS 314 01 - Adv Computer Lang Concepts', 'CIS 332 01 - Web Programming I'}"
+
+if __name__ == "__main__":
+    main()
